8500a37c94d45b32ef43338fc8fcfc6d2ad19497
huhongda <huhongda@fiture.com>
2023-04-09 22:30:19 +0800

    feat(*): remove the old server

--- omni-service/integration-test/src/test/resources/reponse-check.js
+++
@@ -1,7 +0,0 @@
-client.test("request executed successful", function() {
-    client.assert(response.status == 200, "Response status is not 200")
-});
-
-client.test("response body status successful", function() {
-    client.assert(response.body.status == 200, "Response body status is not 200")
-});
